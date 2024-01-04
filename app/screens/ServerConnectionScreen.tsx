21ccc504521ef75939335c0e54d8309f7dd43db3
Stefan Wallin <github@stefan-wallin.se>
2024-01-05 00:53:16 +0100

    App tries to connect to advertised bonjour service

--- app/screens/ServerConnectionScreen.tsx
+++ app/screens/ServerConnectionScreen.tsx
@@ -6,8 +6,11 @@ import { AppStackScreenProps } from "../navigators"
 import { spacing } from "../theme"
 import OnboardingWrapper from "app/components/OnboardingWrapper"
 import { t } from "i18n-js"
+import ServerDiscovery from "app/services/ServerDiscovery"
 
 interface ServerConnectionScreenProps extends AppStackScreenProps<"ServerConnection"> {}
+
+const ServerDiscoveryInstance = new ServerDiscovery()
 
 type Server = {
   id: string
@@ -17,34 +20,50 @@ type Server = {
   status: "ready" | "notFound" | "comingSoon" | "connecting" | "connected"
   disabled?: boolean
 }
-const servers: Server[] = [
-  {
-    id: "1",
-    name: "Marys Macbook Pro",
-    address: "marys-macbook-pro.local",
-    port: 8080,
-    status: "notFound",
-  },
-  {
-    id: "2",
-    name: "Marys PC",
-    address: "192.168.18.1",
-    port: 8080,
-    status: "ready",
-  },
-  {
-    id: "3",
-    name: t("serverConnectionScreen.cloudOption"),
-    address: "cloud.photoback.app",
-    port: 80,
-    disabled: true,
-    status: "comingSoon",
-  },
-]
+// const servers: Server[] = [
+//   {
+//     id: "1",
+//     name: "Marys Macbook Pro",
+//     address: "marys-macbook-pro.local",
+//     port: 8080,
+//     status: "notFound",
+//   },
+//   {
+//     id: "2",
+//     name: "Marys PC",
+//     address: "192.168.18.1",
+//     port: 8080,
+//     status: "ready",
+//   },
+//   {
+//     id: "3",
+//     name: t("serverConnectionScreen.cloudOption"),
+//     address: "cloud.photoback.app",
+//     port: 80,
+//     disabled: true,
+//     status: "comingSoon",
+//   },
+// ]
+type ServerDiscoveryEvent = {
+  status: string
+  result: any
+}
+const updates: ServerDiscoveryEvent[] = []
 
 export const ServerConnectionScreen: FC<ServerConnectionScreenProps> = observer(
   function ServerConnectionScreen(props) {
     const navigation = props.navigation
+
+    React.useEffect(() => {
+      updates.splice(0, updates.length)
+      ServerDiscoveryInstance.startScan((status, result) => {
+        updates.push({ status, result })
+      })
+      return () => {
+        ServerDiscoveryInstance.stopScan()
+      }
+    }, [])
+
     const onServerPress = (item: Server) => {
       navigation.navigate("Login", { server: item.address, port: item.port })
     }
@@ -53,9 +72,12 @@ export const ServerConnectionScreen: FC<ServerConnectionScreenProps> = observer(
         <View>
           <Text preset="heading" style={$text} tx="serverConnectionScreen.header" />
           <Text preset="default" style={$text} tx="serverConnectionScreen.details" />
-          <Text preset="bold" style={$text} tx="serverConnectionScreen.CTA" />
+          {/* <Text preset="bold" style={$text} tx="serverConnectionScreen.CTA" />
           {servers.map((item, index) => {
             return <ServerItem key={item.id} item={item} index={index} onPress={onServerPress} />
+          })} */}
+          {updates.map((item, index) => {
+            return <Text key={`update-${index}`} text={JSON.stringify(item)} />
           })}
         </View>
       </OnboardingWrapper>
